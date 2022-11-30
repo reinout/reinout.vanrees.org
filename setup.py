d4e9473370fc43c1b132809b8c7b3fe6711eaae1
Reinout van Rees <reinout@vanrees.org>
2022-11-30 17:02:20 +0100

    black

--- setup.py
+++ setup.py
@@ -1,22 +1,25 @@
 from distutils.core import setup
 
-install_requires = [
-    'Jinja2',
-    'Sphinx',
-    'Sphinx',
-    'docutils',
-    # 'pastescript',
-    ]
 
-setup(name='rvo',
-      version='0.1',
-      packages=['rvo'],
-      install_requires=install_requires,
-      entry_points={
-        'console_scripts': [
-            'create-weblog-pages = rvo.weblog:main',
-            'create-sermonlog = rvo.sermonlog:main',
-            'create-homepage = rvo.homepage:main',
-            'create-sitemap = rvo.sitemap:main',
-            ]},
-      )
+install_requires = [
+    "Jinja2",
+    "Sphinx",
+    "Sphinx",
+    "docutils",
+    # 'pastescript',
+]
+
+setup(
+    name="rvo",
+    version="0.1",
+    packages=["rvo"],
+    install_requires=install_requires,
+    entry_points={
+        "console_scripts": [
+            "create-weblog-pages = rvo.weblog:main",
+            "create-sermonlog = rvo.sermonlog:main",
+            "create-homepage = rvo.homepage:main",
+            "create-sitemap = rvo.sitemap:main",
+        ]
+    },
+)
